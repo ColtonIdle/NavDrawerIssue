dd182e261e88bd66feced86c81d420a68ed8db0e
ColtonIdle <31751141+ColtonIdle@users.noreply.github.com>
2020-04-17 22:41:28 -0400

    Add missing import

--- app/src/main/java/com/example/navdrawerissue/MainActivity.kt
+++ app/src/main/java/com/example/navdrawerissue/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import dev.chrisbanes.insetter.Insetter
 
 class MainActivity : AppCompatActivity() {
 
